9606a85345975ea88f1d7cc7f64b31c827277b79
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-06-04 18:24:45 -0400

    change max length of name and description properties

--- models/item.js
+++ models/item.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-    name: {type: String, required: true, minLength: 3, maxLength: 100},
-    description: {type: String, maxLength: 250},
+    name: {type: String, required: true, minLength: 3, maxLength: 250},
+    description: {type: String, maxLength: 1000},
     price: {type: Number, min: 0.0},
     number_in_stock: {type: Number, default: 0},
     category:[{type: Schema.Types.ObjectId, ref: 'Category'}]
