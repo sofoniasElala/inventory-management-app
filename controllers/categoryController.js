d308fcec00b4cb3ee72ded2320de1f2525ee5ee3
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-06-05 21:23:28 -0400

    create ejs temps to 'insert' into the layout temp
    temp = template

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,24 +1,32 @@
 const Category = require('../models/category');
 const Item = require('../models/item');
 const asyncHandler = require('express-async-handler');
+const ejs = require('ejs');
+const path = require('path');
 
 exports.category_list = asyncHandler(async(req, res, next) => {
     const categories = await Category.find().sort({name: 1}).exec();
-    res.render('category_list', {
+    const allCategoriesEJS = await ejs.renderFile(path.join(__dirname, '../views/category_list.ejs'), {
         title: 'All categories',
         categories: categories
+    })
+    res.render('layout', {
+        body: allCategoriesEJS
     });
 });
 
 exports.category_detail = asyncHandler(async (req, res, next) => {
-    const [category, allItemsByCategory] = Promise.all([
-        await Category.findById(`${req.params.id}`).exec(),
-        await Item.find({category: req.params.id}, 'name description').exec()
+    const [category, allItemsByCategory] = await Promise.all([
+         Category.findById(`${req.params.id}`).exec(),
+         Item.find({category: req.params.id}, 'name description').exec()
     ]);
-
-    res.render('category_detail', {
+    const categoryDetailEJS = await ejs.renderFile(path.join(__dirname, '../views/category_detail.ejs'), {
         category,
         items: allItemsByCategory
+    })
+
+    res.render('layout', {
+        body: categoryDetailEJS
     });
 
 })
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,22 +1,29 @@
 const Item = require('../models/item');
 const Category = require('../models/category');
 const asyncHandler = require('express-async-handler');
+const ejs = require('ejs');
+const path = require('path');
 
 
 // list of all items
 exports.item_list = asyncHandler(async (req, res, next) => {
-    const Allitems = Item.find({}, 'name description').sort({name: 1}).populate('category').exec();
-    res.render('item_list', {
+    const Allitems = await Item.find({}).sort({name: 1}).populate('category').exec();
+    const allItemsEJS = await ejs.renderFile(path.join(__dirname, '../views/item_list.ejs'), {
         title: 'All items',
         items: Allitems
+    })
+    res.render('layout', {
+        body: allItemsEJS
     });
 });
 
 exports.item_detail = asyncHandler(async (req, res, next) => {
-    const item = Item.findById(`${req.params.id}`).populate('category').exec();
-
-    res.render('item_detail', {
-        title: 'Item detail',
+    const item = await Item.findById(`${req.params.id}`).populate('category').exec();
+    const itemDetailEJS = await ejs.renderFile(path.join(__dirname, '../views/item_detail.ejs'), {
         item,
+    })
+
+    res.render('layout', {
+        body: itemDetailEJS
     });
 });
